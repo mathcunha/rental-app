dfb458519570d58fe31606bd6e73418a1143ca94
Matheus Cunha <chellou@hotmail.com>
2020-06-11 12:41:26 +0000

    implemented rent security

--- api/src/main/java/com/rental/api/event/RentEventHandler.java
+++ api/src/main/java/com/rental/api/event/RentEventHandler.java
@@ -3,24 +3,21 @@ package com.rental.api.event;
 import com.rental.api.domain.Apartment;
 import com.rental.api.domain.Rent;
 import com.rental.api.repository.ApartmentRepository;
-import com.rental.api.security.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 @RepositoryEventHandler
 @Component
 public class RentEventHandler {
-    @PersistenceContext
-    private EntityManager em;
+    @Autowired
+    ApartmentRepository aptRepository;
+    //https://stackoverflow.com/questions/1972933/cross-field-validation-with-hibernate-validator-jsr-303
 
     @HandleBeforeCreate
     public void handleBeforeSave(Rent rent) {
-        Apartment apt = em.find(Apartment.class, rent.getApartment().getApartmentId());
+        Apartment apt = aptRepository.findToRent(rent.getApartment().getApartmentId(), "publicApartment");
         if(!rent.getApartment().getPrice().equals(apt.getPrice())){
             rent.getApartment().setPrice(null);
         }
