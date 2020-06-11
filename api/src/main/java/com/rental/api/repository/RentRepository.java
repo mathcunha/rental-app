80f1b48821566cc1eb2b72d2a24075582de2fc78
Matheus Cunha <chellou@hotmail.com>
2020-06-11 12:40:55 +0000

    implemented rent security

--- api/src/main/java/com/rental/api/repository/RentRepository.java
+++ api/src/main/java/com/rental/api/repository/RentRepository.java
@@ -3,10 +3,12 @@ package com.rental.api.repository;
 import com.rental.api.domain.Rent;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(exported = true)
 @CrossOrigin
 public interface RentRepository extends Repository<Rent, Long> {
+    @PreAuthorize("#rent.id == null && #rent.user.id == authentication.principal.id")
     Rent save(Rent rent);
 }
